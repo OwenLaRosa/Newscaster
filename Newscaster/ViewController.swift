0800fea21e387dad5217b965d42e258cbc4cee6c
OwenLaRosa <owen.larosa@outlook.com>
2016-01-23 13:39:07 -0500

    Initial Commit

---
+++ Newscaster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Newscaster
+//
+//  Created by Owen LaRosa on 1/23/16.
+//  Copyright © 2016 Owen LaRosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ NewscasterTests/NewscasterTests.swift
@@ -0,0 +1,36 @@
+//
+//  NewscasterTests.swift
+//  NewscasterTests
+//
+//  Created by Owen LaRosa on 1/23/16.
+//  Copyright © 2016 Owen LaRosa. All rights reserved.
+//
+
+import XCTest
+@testable import Newscaster
+
+class NewscasterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
