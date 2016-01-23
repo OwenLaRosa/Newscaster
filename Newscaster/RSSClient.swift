b4341e6f64580d501365869659c748590d86794d
OwenLaRosa <owen.larosa@outlook.com>
2016-01-23 14:04:30 -0500

    Add generic downloading/parsing methods

--- Newscaster/RSSClient.swift
+++ Newscaster/RSSClient.swift
@@ -10,6 +10,29 @@ import Foundation
 
 class RSSClient {
     
+    /// Download JSON data at the destination URL.
+    private func downloadJSONData(url: String, completionHandler: (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void) -> NSURLSessionTask {
+        let session = NSURLSession.sharedSession()
+        let request = NSMutableURLRequest(URL: NSURL(string: url)!)
+        request.HTTPMethod = "GET"
+        
+        let task = session.dataTaskWithRequest(request) {data, response, error in
+            completionHandler(data: data, response: response, error: error)
+        }
+        task.resume()
+        
+        return task
+    }
     
+    /// Attempt to convert JSON data into NSDictionary.
+    private func parseJSONData(data: NSData, completionHandler: (result: NSDictionary?, error: NSError?) -> Void) {
+        do {
+            if let parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments) as? NSDictionary {
+                completionHandler(result: parsedResult, error: nil)
+            }
+        } catch let parsingError as NSError {
+            completionHandler(result: nil, error: parsingError)
+        }
+    }
     
 }
