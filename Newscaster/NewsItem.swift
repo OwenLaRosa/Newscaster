a96b1b25ecf87183b4a569cdca3a23c624f45ad6
OwenLaRosa <owen.larosa@outlook.com>
2016-01-27 16:49:36 -0500

    Add struct for news item

---
+++ Newscaster/NewsItem.swift
@@ -0,0 +1,13 @@
+//
+//  NewsItem.swift
+//  Newscaster
+//
+//  Created by Owen LaRosa on 1/27/16.
+//  Copyright © 2016 Owen LaRosa. All rights reserved.
+//
+
+import Foundation
+
+struct NewsItem {
+    
+}
