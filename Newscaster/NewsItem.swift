dab794d615bfad5584d751a3e3bbe55736280700
OwenLaRosa <owen.larosa@outlook.com>
2016-01-27 16:56:15 -0500

    Add properties and RSS initializer

--- Newscaster/NewsItem.swift
+++ Newscaster/NewsItem.swift
@@ -10,4 +10,23 @@ import Foundation
 
 struct NewsItem {
     
+    let title: String
+    let description: String
+    let link: String
+    let date: NSDate
+    
+    init(rss: [String : AnyObject]) {
+        self.title = rss["title"] as? String ?? ""
+        self.description = rss["description"] as? String ?? ""
+        self.link = rss["link"] as? String ?? ""
+        // TODO: Get the date from the result
+        self.date = NSDate()
+    }
+    
+    
+    
+    
+    
+    
+    
 }
