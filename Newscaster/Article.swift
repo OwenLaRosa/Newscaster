302319377804377ffc18546598173bf27999e881
OwenLaRosa <owen.larosa@outlook.com>
2016-01-28 16:34:32 -0500

    Implement subclass for Article entity

--- Newscaster/Article.swift
+++ Newscaster/Article.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+import CoreData
+
+@objc class Article: NSManagedObject {
+    
+    /// The title of the article
+    @NSManaged var title: String
+    /// The URL where the article is located
+    @NSManaged var link: String
+    /// The author or site who published the article
+    @NSManaged var source: String?
+    /// Preview text of the article
+    @NSManaged var preview: String?
+    /// The article's publication date
+    @NSManaged var date: NSDate?
+    /// Plaintext content of the article
+    @NSManaged var textContent: String?
+    
+    override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+    }
+    
+    init(newsItem: NewsItem, context: NSManagedObjectContext) {
+        let entity = NSEntityDescription.entityForName("Article", inManagedObjectContext: context)!
+        
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+        
+        self.title = newsItem.title
+        self.link = newsItem.link
+        //self.source = newsItem.source
+        self.preview = newsItem.description
+        self.date = newsItem.date
+    }
+    
+}
