679b5a68bd6e455a14b3c9600a89d382ea2f8e40
OwenLaRosa <owen.larosa@outlook.com>
2016-01-31 11:58:47 -0500

    Rename default view controller

--- Newscaster/FeedViewController.swift
+++ Newscaster/FeedViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FeedViewController.swift
 //  Newscaster
 //
 //  Created by Owen LaRosa on 1/23/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FeedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
