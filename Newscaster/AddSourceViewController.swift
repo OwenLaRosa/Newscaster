31e871d4ae738b42f03ee81318becd760c2ecd1f
OwenLaRosa <owen.larosa@outlook.com>
2016-01-31 16:19:09 -0500

    Implement logic for adding sources

--- Newscaster/AddSourceViewController.swift
+++ Newscaster/AddSourceViewController.swift
@@ -10,4 +10,43 @@ import UIKit
 
 class AddSourceViewController: UIViewController {
     
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var linkField: UITextField!
+    
+    @IBAction func addFeed(sender: UIButton) {
+        if let name = nameField.text {
+            var type = "google" // default value is a text search
+            var link: String? // nil if a search query is used
+            var query: String?
+            // check if the link is a valid url
+            if validateUrl(linkField.text!) {
+                type = "none" // atom or rss will be determined later
+                link = linkField.text!
+            } else {
+                // otherwise, the text entry becomes the search query
+                query = linkField.text!
+            }
+            let feed = Feed(type: type, query: query, url: link, context: sharedContext)
+            saveContext()
+        }
+        dismissViewControllerAnimated(false, completion: nil)
+    }
+    
+    @IBAction func cancel(sender: UIButton) {
+        // return to the feeds view controller
+        dismissViewControllerAnimated(false, completion: nil)
+    }
+    
+    /// Determine if the url is correctly formatted.
+    private func validateUrl(url: String) -> Bool {
+        // regular url format
+        let pattern = "^(https?:\\/\\/)([a-zA-Z0-9_\\-~]+\\.)+[a-zA-Z0-9_\\-~\\/\\.]+$"
+        // check if string conforms to format
+        if let _ = url.rangeOfString(pattern, options: .RegularExpressionSearch) {
+            return true
+        }
+        return false
+    }
+    
 }
