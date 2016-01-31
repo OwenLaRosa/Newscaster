6a1c8395ab0672656c03101f5a6deb9e6417146a
OwenLaRosa <owen.larosa@outlook.com>
2016-01-31 12:56:47 -0500

    Add custom classes for table view cells

---
+++ Newscaster/ArticleTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleTableViewCell.swift
+//  Newscaster
+//
+//  Created by Owen LaRosa on 1/31/16.
+//  Copyright © 2016 Owen LaRosa. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var headlineLabel: UILabel!
+    @IBOutlet weak var linkLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
+    
+}
---
+++ Newscaster/FeedTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedTableViewCell.swift
+//  Newscaster
+//
+//  Created by Owen LaRosa on 1/31/16.
+//  Copyright © 2016 Owen LaRosa. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var feedNameLabel: UILabel!
+    @IBOutlet weak var feedDescriptionLabel: UILabel!
+    
+}
