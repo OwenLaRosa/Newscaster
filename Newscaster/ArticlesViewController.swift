21cbfce3ad06d2cbafc9f467b8e6a94a5d23e154
OwenLaRosa <owen.larosa@outlook.com>
2016-02-02 16:46:49 -0500

    Add data source and fetched results controller

--- Newscaster/ArticlesViewController.swift
+++ Newscaster/ArticlesViewController.swift
@@ -7,7 +7,74 @@
 //
 
 import UIKit
+import CoreData
 
-class ArticlesViewController: UIViewController {
+class ArticlesViewController: UIViewController, UITableViewDataSource, NSFetchedResultsControllerDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var feed: Feed!
+    var articles = [Article]()
+    var insertedIndexPaths = [NSIndexPath]()
+    var deletedIndexPaths = [NSIndexPath]()
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fetchedResultsController.fetchedObjects?.count ?? 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ArticleTableViewCell")! as! ArticleTableViewCell
+        let article = fetchedResultsController.objectAtIndexPath(indexPath) as! Article
+        
+        cell.headlineLabel.text = article.title
+        cell.linkLabel.text = article.link
+        // details about source and publication date
+        cell.detailTextLabel?.text = ""
+        
+        return cell
+    }
+    
+    lazy var fetchedResultsController: NSFetchedResultsController = {
+        let fetchRequest = NSFetchRequest(entityName: "Article")
+        fetchRequest.sortDescriptors = []
+        fetchRequest.predicate = NSPredicate(format: "feed == %@", self.feed)
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: sharedContext, sectionNameKeyPath: nil, cacheName: nil)
+        return fetchedResultsController
+    }()
+    
+    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+        insertedIndexPaths.removeAll()
+        deletedIndexPaths.removeAll()
+    }
+    
+    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+        switch type {
+        case .Insert:
+            insertedIndexPaths.append(newIndexPath!)
+        case .Delete:
+            deletedIndexPaths.append(indexPath!)
+        default:
+            return
+        }
+    }
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        tableView.beginUpdates()
+        tableView.insertRowsAtIndexPaths(insertedIndexPaths, withRowAnimation: .Fade)
+        tableView.deleteRowsAtIndexPaths(deletedIndexPaths, withRowAnimation: .Fade)
+        tableView.endUpdates()
+        
+        saveContext()
+    }
     
 }
