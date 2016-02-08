a8d8a929b2a433fe2eb55a2d13ac565f2b73b37b
OwenLaRosa <owen.larosa@outlook.com>
2016-02-08 18:40:47 -0500

    Load data into web view

--- Newscaster/LinkViewController.swift
+++ Newscaster/LinkViewController.swift
@@ -18,6 +18,13 @@ class LinkViewController: UIViewController {
         super.viewWillAppear(animated)
         
         navigationItem.title = article.title
+        NewsClient().downloadData(article.link) {data, response, error in
+            if let htmlData = data {
+                if let htmlString = String(data: htmlData, encoding: NSUTF8StringEncoding) {
+                    self.webView.loadHTMLString(htmlString, baseURL: NSURL(string: self.article.link))
+                }
+            }
+        }
     }
     
 }
