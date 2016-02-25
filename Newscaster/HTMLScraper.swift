f46753d75d0b24ee34661dd8da645296571ddcb0
OwenLaRosa <owen.larosa@outlook.com>
2016-02-25 15:14:30 -0500

    Create HTMLScraper class

---
+++ Newscaster/HTMLScraper.swift
@@ -0,0 +1,13 @@
+//
+//  HTMLScraper.swift
+//  Newscaster
+//
+//  Created by Owen LaRosa on 2/25/16.
+//  Copyright © 2016 Owen LaRosa. All rights reserved.
+//
+
+import Foundation
+
+class HTMLScraper {
+    
+}
