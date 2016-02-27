e00f40b8b1171b16e8d7d7b43b44f40bb2b3f8d4
OwenLaRosa <owen.larosa@outlook.com>
2016-02-27 14:11:43 -0500

    Add Root entity

---
+++ Newscaster/Root.swift
@@ -0,0 +1,22 @@
+//
+//  Root.swift
+//  Newscaster
+//
+//  Created by Owen LaRosa on 2/27/16.
+//  Copyright © 2016 Owen LaRosa. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc class Root: NSManagedObject {
+    
+    /// Ordered set of all Feed entities
+    @NSManaged var feeds: NSOrderedSet
+    
+    init(context: NSManagedObjectContext) {
+        let entity = NSEntityDescription.entityForName("Root", inManagedObjectContext: context)!
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+    }
+    
+}
