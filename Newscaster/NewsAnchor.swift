42e2a22b19b1805b8e02cdf76f2ef4942be26503
OwenLaRosa <owen.larosa@outlook.com>
2016-03-26 11:19:25 -0400

    Add code to speak individual words

--- Newscaster/NewsAnchor.swift
+++ Newscaster/NewsAnchor.swift
@@ -13,6 +13,14 @@ public class NewsAnchor: AVSpeechSynthesizer {
     /// String that will be spoken
     var stringToSpeak: String!
     
+    /// Individual words, defined by spaces, in the speech string
+    private var words: [String] {
+        return stringToSpeak.componentsSeparatedByString(" ")
+    }
+    
+    /// Index of the word to be spoken
+    var speechIndex = 0
+    
     /// Start speaking the string from the beginning.
     public func startSpeaking() {
         
@@ -26,6 +34,21 @@ public class NewsAnchor: AVSpeechSynthesizer {
     /// Resume speech from the point in which is was paused
     public func resumeSpeaking() {
         
+    }
+    
+    /// Speak the word at the next index
+    private func speakAtIndex(index: Int) {
+        if index > words.count - 1 {
+            // index out of range, nothing to speak
+            // reset the index
+            speechIndex = 0
+            return
+        }
+        let word = words[index]
+        let utterance = AVSpeechUtterance(string: word)
+        // use the iOS "Daniel" voice
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-uk")
+        speakUtterance(utterance)
     }
     
 }
