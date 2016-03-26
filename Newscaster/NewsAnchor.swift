310a7f76e0d86d64f4d2210ec6eeb3d6c45a0258
OwenLaRosa <owen.larosa@outlook.com>
2016-03-26 10:29:30 -0400

    Add NewsAnchor class

---
+++ Newscaster/NewsAnchor.swift
@@ -0,0 +1,13 @@
+//
+//  NewsAnchor.swift
+//  Newscaster
+//
+//  Created by Owen LaRosa on 3/26/16.
+//  Copyright © 2016 Owen LaRosa. All rights reserved.
+//
+
+import AVFoundation
+
+class NewsAnchor: AVSpeechSynthesizer {
+    
+}
