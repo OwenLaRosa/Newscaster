d5744e91c2b1286389a0031ea71dbf2b1de8b353
OwenLaRosa <owen.larosa@outlook.com>
2016-03-30 16:45:23 -0400

    Remove delegate and speakAtIndex

--- Newscaster/NewsAnchor.swift
+++ Newscaster/NewsAnchor.swift
@@ -13,20 +13,6 @@ public class NewsAnchor: AVSpeechSynthesizer {
     /// String that will be spoken
     var stringToSpeak: String!
     
-    /// Individual words, defined by spaces, in the speech string
-    private var words: [String] {
-        return stringToSpeak.componentsSeparatedByString(" ")
-    }
-    
-    /// Index of the word to be spoken
-    var speechIndex = 0
-    
-    override init() {
-        super.init()
-        
-        delegate = self
-    }
-    
     /// Start speaking the string from the beginning.
     public func startSpeaking() {
         let utterance = AVSpeechUtterance(string: stringToSpeak)
@@ -43,31 +29,6 @@ public class NewsAnchor: AVSpeechSynthesizer {
     /// Resume speech from the point in which is was paused
     public func resumeSpeaking() {
         continueSpeaking()
-    }
-    
-    /// Speak the word at the next index
-    private func speakAtIndex(index: Int) {
-        if index > words.count - 1 {
-            // index out of range, nothing to speak
-            // reset the index
-            speechIndex = 0
-            return
-        }
-        let word = words[index]
-        let utterance = AVSpeechUtterance(string: word)
-        // use the iOS "Daniel" voice
-        utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
-        speakUtterance(utterance)
-    }
-    
-}
-
-extension NewsAnchor: AVSpeechSynthesizerDelegate {
-    
-    public func speechSynthesizer(synthesizer: AVSpeechSynthesizer, didFinishSpeechUtterance utterance: AVSpeechUtterance) {
-        // go to the next word
-        speechIndex++
-        speakAtIndex(speechIndex)
     }
     
 }
