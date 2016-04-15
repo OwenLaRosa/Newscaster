be0966ca6c7d5e9cd3f0feb8dd5f82ff252a6a42
OwenLaRosa <owen.larosa@outlook.com>
2016-04-15 15:58:38 -0400

    Add custom settings table view cells

---
+++ Newscaster/SpeechVoiceTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SpeechVoiceTableViewCell.swift
+//  Newscaster
+//
+//  Created by Owen LaRosa on 4/15/16.
+//  Copyright © 2016 Owen LaRosa. All rights reserved.
+//
+
+import UIKit
+
+class SpeechVoiceTableViewCell: UITableViewCell {
+    
+}
