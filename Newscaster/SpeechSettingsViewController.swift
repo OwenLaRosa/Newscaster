4cb8c821af66bac559b2005f052fb6137a7fa44b
OwenLaRosa <owen.larosa@outlook.com>
2016-04-15 15:30:48 -0400

    Add class for speech settings view controller

---
+++ Newscaster/SpeechSettingsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SpeechSettingsViewController.swift
+//  Newscaster
+//
+//  Created by Owen LaRosa on 4/15/16.
+//  Copyright © 2016 Owen LaRosa. All rights reserved.
+//
+
+import UIKit
+
+class SpeechSettingsViewController: UIViewController {
+    
+    
+    
+}
