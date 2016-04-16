cf18a7d76b63160c7b5634c5e4d28f8f034320c1
OwenLaRosa <owen.larosa@outlook.com>
2016-04-16 12:01:38 -0400

    Add Settings class

---
+++ Newscaster/Settings.swift
@@ -0,0 +1,71 @@
+//
+//  Settings.swift
+//  Newscaster
+//
+//  Created by Owen LaRosa on 4/16/16.
+//  Copyright © 2016 Owen LaRosa. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    
+    private var userDefaults = NSUserDefaults.standardUserDefaults()
+    
+    private struct Defaults {
+        static let voice = "en-GB"
+        static let rate = 1.0
+        static let pitch = 1.0
+    }
+    
+    private struct Keys {
+        static let voice = "voice"
+        static let rate = "rate"
+        static let pitch = "pitch"
+    }
+    
+    var voice: String {
+        get {
+            return userDefaults.stringForKey(Keys.voice) ?? Defaults.voice
+        }
+        set (newVoice) {
+            userDefaults.setObject(newVoice, forKey: Keys.voice)
+        }
+    }
+    
+    var rate: Double {
+        get {
+            let rateValue = userDefaults.doubleForKey(Keys.rate)
+            if rateValue > 0 {
+                return rateValue
+            }
+            return Defaults.rate
+        }
+        set (newRate) {
+            userDefaults.setObject(newRate, forKey: Keys.rate)
+        }
+    }
+    
+    var pitch: Double {
+        get {
+            let pitchValue = userDefaults.doubleForKey(Keys.pitch)
+            if pitchValue > 0 {
+                return pitchValue
+            }
+            return Defaults.pitch
+        }
+        set (newPitch) {
+            userDefaults.setDouble(newPitch, forKey: Keys.pitch)
+        }
+    }
+    
+    class func sharedInstance() -> Settings {
+        
+        struct Singleton {
+            static var sharedInstance = Settings()
+        }
+        
+        return Singleton.sharedInstance
+    }
+    
+}
